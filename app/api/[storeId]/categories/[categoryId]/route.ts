afbd7c3f972378b60afa4f6b51d5da9719935515
Admin <support@unlimitpotential.com>
2023-07-26 15:24:54 -0400

    f

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -2,7 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { categoryId: string } }) {
+export async function GET(req: Request, { params }: { params: { categoryId: number } }) {
   try {
     if (!params.categoryId) {
       return new NextResponse("Category id is required", { status: 400 });
