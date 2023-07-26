2c05663e3e4355f29fb6fe809aaf850a2173f456
Admin <support@unlimitpotential.com>
2023-07-26 16:23:06 -0400

    g

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Head from "next/head";
 import { SignedIn, currentUser, SignedOut, SignOutButton, useAuth } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
