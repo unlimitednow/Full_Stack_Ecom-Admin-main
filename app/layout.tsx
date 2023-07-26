76a6c37b078098f28acf9ffc4d17771920e9226e
Admin <support@unlimitpotential.com>
2023-07-26 16:47:43 -0400

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,6 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Admin Dashboard",
-  description: "Admin Dashboard",
-};
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
